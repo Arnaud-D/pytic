46199d938b9dc4c100bdb60c200a0109a837450d
Arnaud-D <35631001+Arnaud-D@users.noreply.github.com>
2019-12-07 23:21:25 +0100

    Viewer: switch to numpy arrays instead of lists

--- viewer/analyzer.py
+++ viewer/analyzer.py
@@ -1,14 +1,20 @@
-import json
 import matplotlib.pyplot as plt
-from scipy.signal import savgol_filter
+import scipy.signal
+import numpy as np
 import tic_parser
 
+# Unit conversions
+j_per_wh = 3.6e6  # J / Wh
+s_per_ms = 0.001  # s / ms
+kwh_per_wh = 0.001  # kWh / Wh
+kwh_per_j = 1 / 3.6e6  # kWh / J
 
-def create(meter_mode, data_filename, time_filename):
+
+def create(meter_mode, data_filename, time_filename=None):
     parser = tic_parser.create(meter_mode, data_filename, time_filename)
     frames = parser.parse()
     analyzer = Analyzer()
-    analyzer.datastore = JsonHistoricDataStore(frames)
+    analyzer.datastore = HistoricDatastore(frames)
     analyzer.analyze()
     return analyzer
 
@@ -16,15 +22,13 @@ def create(meter_mode, data_filename, time_filename):
 class Analyzer:
     @staticmethod
     def compute_avgpower(index_values, time):
-        # Filter index values using Savitzky Golay filter. We pretend that the data points are equally spaced.
+        # Compute power by using a derivating Savitzky Golay filter on the index.
+        # We pretend that the data points are equally spaced.
         # 195 samples <=> 5 min of data @ 1.55 frames/s
-        dtime = [time[i+1] - time[i] for i in range(len(time)-1)]
-        delta = sum(dtime) / len(dtime)
-        deriv_index_values_f = savgol_filter(index_values, 195, 2, deriv=1, delta=delta)
-        j_per_wh = 3.6e6  # joules per watt-hour
-        deriv_index_values = [d * j_per_wh for d in deriv_index_values_f]
-        avgpower_validity = [True] * len(deriv_index_values)
-        return time, deriv_index_values, avgpower_validity
+        dt = np.average(np.diff(time))
+        avgpower = scipy.signal.savgol_filter(index_values, window_length=195, polyorder=2, deriv=1, delta=dt)
+        avgpower_validity = np.full_like(avgpower, True)
+        return time, avgpower * j_per_wh, avgpower_validity
 
     def __init__(self):
         self.power = None
@@ -33,9 +37,9 @@ class Analyzer:
         self.datastore = None
 
     def analyze(self):
-        time, _ = self.datastore.select('timestamp')
-        power_values, power_validity = self.datastore.select('PAPP')
-        index_values, index_validity = self.datastore.select('BASE')
+        time, _ = self.datastore.get_field('timestamp')
+        power_values, power_validity = self.datastore.get_field('PAPP')
+        index_values, index_validity = self.datastore.get_field('BASE')
 
         # Compute derived data
         time_avgpower, avgpower_values, avgpower_validity = self.compute_avgpower(index_values, time)
@@ -45,48 +49,37 @@ class Analyzer:
         self.avgpower = TimeSeries(time_avgpower, avgpower_values, avgpower_validity)
 
     def get_figure_power(self, width, height, dpi):
-        figure = plt.Figure(figsize=(width / dpi, height / dpi), dpi=dpi)
-        ax = figure.add_subplot(111)
-        ax.plot(self.power.time, self.power.values)
-        ax.set_xlabel("Temps (s)")
-        ax.set_ylabel("Puissance apparente (VA)")
-        invalid_data = [self.power.values[i] for i in range(len(self.power.validity)) if not self.power.validity[i]]
-        invalid_time = [self.power.time[i] for i in range(len(self.power.validity)) if not self.power.validity[i]]
-        figure.gca().plot(invalid_time, invalid_data, 'r. ')
-        return figure
+        return Analyzer.get_figure_with_time(width, height, dpi, self.power, "Puissance apparente (VA)")
 
     def get_figure_index(self, width, height, dpi):
-        figure = plt.Figure(figsize=(width / dpi, height / dpi), dpi=dpi)
-        ax = figure.add_subplot(111)
-        ax.plot(self.index.time, self.index.values)
-        ax.set_xlabel("Temps (s)")
-        ax.set_ylabel("Index (kWh)")
-        invalid_data = [self.index.values[i] for i in range(len(self.index.validity)) if not self.index.validity[i]]
-        invalid_time = [self.index.time[i] for i in range(len(self.index.validity)) if not self.index.validity[i]]
-        figure.gca().plot(invalid_time, invalid_data, 'r. ')
-        return figure
+        return Analyzer.get_figure_with_time(width, height, dpi, self.index, "Index (kWh)")
 
     def get_figure_avgpower(self, width, height, dpi):
+        return Analyzer.get_figure_with_time(width, height, dpi, self.avgpower, "Puissance moyenne (W)")
+
+    @staticmethod
+    def get_figure_with_time(width, height, dpi, timeseries, ylabel):
         figure = plt.Figure(figsize=(width/dpi, height/dpi), dpi=dpi)
-        ax = figure.add_subplot(111)
-        ax.plot(self.avgpower.time, self.avgpower.values)
+        ax = figure.add_subplot(1, 1, 1)
+        ax.plot(timeseries.time, timeseries.values)
         ax.set_xlabel("Temps (s)")
-        ax.set_ylabel("Puissance moyenne (W)")
-        invalid_data = [self.avgpower.values[i] for i in range(len(self.avgpower.validity)) if not self.avgpower.validity[i]]
-        invalid_time = [self.avgpower.time[i] for i in range(len(self.avgpower.validity)) if not self.avgpower.validity[i]]
+        ax.set_ylabel(ylabel)
+        invalid_indices = np.logical_not(timeseries.validity)
+        invalid_data = timeseries.values[invalid_indices]
+        invalid_time = timeseries.time[invalid_indices]
         figure.gca().plot(invalid_time, invalid_data, 'r. ')
         return figure
 
     def get_figure_hist_power_time(self, width, height, dpi):
         figure = plt.Figure(figsize=(width / dpi, height / dpi), dpi=dpi)
         ax = figure.add_subplot(2, 1, 1)
-        dtime = [self.avgpower.time[i+1] - self.avgpower.time[i] for i in range(len(self.avgpower.time)-1)]
-        ax.hist(self.avgpower.values[:-1], bins=range(0, 6000, 200), weights=dtime)
+        dt = np.diff(self.avgpower.time)
+        ax.hist(self.avgpower.values[:-1], bins=range(0, 6000, 200), weights=dt)
         ax.set_xlabel("Puissance moyenne (W)")
         ax.set_ylabel("Durée (s)")
 
         ax = figure.add_subplot(2, 1, 2)
-        ax.hist(self.avgpower.values[:-1], bins=range(0, 6000, 50), weights=dtime, cumulative=True, density=True)
+        ax.hist(self.avgpower.values[:-1], bins=range(0, 6000, 50), weights=dt, cumulative=True, density=True)
         ax.grid()
         ax.set_ylabel("Durée cumulée normalisée")
         ax.set_xlabel("Puissance moyenne (W)")
@@ -95,8 +88,7 @@ class Analyzer:
     def get_figure_hist_power_energy(self, width, height, dpi):
         figure = plt.Figure(figsize=(width / dpi, height / dpi), dpi=dpi)
         ax = figure.add_subplot(2, 1, 1)
-        kwh_per_j = 1/3.6e6
-        energy = [(self.avgpower.time[i+1] - self.avgpower.time[i])*self.avgpower.values[i]*kwh_per_j for i in range(len(self.avgpower.time)-1)]
+        energy = np.diff(self.avgpower.time) * self.avgpower.values[:-1] * kwh_per_j
         ax.hist(self.avgpower.values[:-1], bins=range(0, 6000, 50), weights=energy)
         ax.set_ylabel("Énergie (kWh)")
 
@@ -115,78 +107,32 @@ class TimeSeries:
         self.validity = validity
 
 
-class JsonHistoricDataStore:
-    @staticmethod
-    def load(filename):
-        """Load data from a file."""
-        frames = []
-        with open(filename, "r") as f:
-            for line in f.readlines():
-                frames.append(json.loads(line))
-        return frames
-
+class HistoricDatastore:
     def __init__(self, frames):
         self.frames = frames
+        self.length = len(self.frames)
+        self.timestamp = self.extract('timestamp', s_per_ms, float)
+        self.papp = self.extract('PAPP', 1, float)
+        self.base = self.extract('BASE', kwh_per_wh, float)
 
-    def select(self, field):
+    def get_field(self, field):
         if field == "timestamp":
-            return self.select_timestamp()
+            return self.timestamp
         elif field == "PAPP":
-            return self.select_papp()
+            return self.papp
         elif field == "BASE":
-            return self.select_base()
+            return self.base
         else:
-            raise KeyError
+            raise ValueError(field)
 
-    def select_timestamp(self):
-        time = []
-        validity = []
-        s_per_ms = 0.001
-        for frame in self.frames:
-            time.append(frame['timestamp']['data'] * s_per_ms)
-            validity.append(frame['timestamp']['valid'])
-        return time, validity
-
-    def select_papp(self):
-        papp = []
-        validity = []
-        for frame in self.frames:
+    def extract(self, field, scaling, converter):
+        data = np.zeros(self.length)
+        validity = np.full(self.length, False)
+        for k, frame in zip(range(self.length), self.frames):
             try:
-                if frame['PAPP']['valid']:
-                    papp.append(int(frame['PAPP']['data']))
-                else:
-                    papp.append(papp[-1])
-                validity.append(frame['PAPP']['valid'])
-            except (ValueError, KeyError):
-                if papp:
-                    papp.append(papp[-1])
-                papp.append(papp[-1])
-                validity.append(False)
-        return papp, validity
-
-    def select_base(self):
-        base = []
-        validity = []
-        kwh_per_wh = 0.001
-        for frame in self.frames:
-            try:
-                if frame['BASE']['valid']:
-                    base.append(int(frame['BASE']['data']) * kwh_per_wh)
-                else:
-                    base.append(base[-1])
-                validity.append(frame['BASE']['valid'])
-            except (ValueError, KeyError):
-                if base:
-                    base.append(base[-1])
-                validity.append(False)
-        return base, validity
-
-
-class JsonHistoricAnalyzer(Analyzer):
-    def __init__(self, filename):
-        super().__init__()
-        self.datastore = JsonHistoricDataStore(filename)
-        self.analyze()
-
-    def analyze(self):
-        raise NotImplementedError
+                validity[k] = frame[field]['valid']
+                data[k] = converter(frame[field]['data']) if validity[k] else data[k-1]
+            except (ValueError, KeyError):  # Field does not exist or cannot be converted
+                data[k] = data[k-1]
+                # validity[k] already false by default
+        return data * scaling, validity
